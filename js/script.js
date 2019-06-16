7bdf28cad901fb502710898c5cdb77f3c7703060
AbdussamadYisau <Sammieyisau@gmail.com>
2019-06-16 03:22:21 +0100

    Fixed the background Image responsiveness issue. Real glad about that!

--- js/script.js
+++ js/script.js
@@ -2,21 +2,22 @@
 const time = document.getElementById('time');
 const greeting = document.getElementById('greeting');
 const name = document.getElementById('name');
-const focus = document.getElementById('greeting');
+const focus = document.getElementById('focus');
+
 
   //Show Time
 
   function showTime() {
-    // body...
-    let today = new Date(),
-      hour = today.getHours(),
-      min  = today.getMinutes(),
-      sec  = today.getSeconds();
+  	// body...
+  	let today = new Date(),
+  	  hour = today.getHours(),
+  	  min  = today.getMinutes(),
+  	  sec  = today.getSeconds();
 
-    // Set AM or PM
-    const amPm = hour >= 12 ? 'PM' : 'AM' ;
+  	// Set AM or PM
+  	const amPm = hour >= 12 ? 'PM' : 'AM' ;
 
-    //12hr Format
+  	//12hr Format
       hour = hour % 12 || 12;
 
       //Output Time
@@ -27,41 +28,67 @@ const focus = document.getElementById('greeting');
   }
 // Add Zeros
 function addZero(n) {
-  return (parseInt(n,10) < 10 ? '0' : '') + n;
+	return (parseInt(n,10) < 10 ? '0' : '') + n;
 }
 
 // Set Background and Greeting
 function setBG() {
-  let today = new Date(), 
-     hour = today.getHours();
+	let today = new Date(), 
+	   hour = today.getHours();
     
     if (hour < 12) {
-      // Morning
-      //document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
-      document.body.style.backgroundImage = "url('./img/morning.jpg')";
-      greeting.textContent = 'Good Morning,' ;  
+    	// Morning
+    	
+    	document.body.style.backgroundImage = "url('./img/morning.jpg')";
+    	// document.body.style.color= '#FFD700';
+      greeting.textContent = 'Good Morning,' ;	
     }
 
     else if (hour < 18) {
-      // Afternoon
+    	// Afternoon
 
-      document.body.style.backgroundImage = "url('./img/afternoon.jpg')";
+       	document.body.style.backgroundImage = "url('./img/afternoon.jpg')";
         greeting.textContent = 'Good Afternoon,' ;
 
     }
 
     else {
-      //Evening  
+    	//Evening  
 
-      document.body.style.backgroundImage = "url('./img/night.jpg')";
-        greeting.textContent = 'Good Evening,' ;  
+    	document.body.style.backgroundImage = "url('./img/night.jpg')";
+
+        greeting.textContent = 'Good Evening,' ;	
 
     }
 
 }
 
-   
+//Get Name
+function getName() {
+   if (localStorage.getItem('name') === null) {
+     name.textContent = '[Enter Name]' ; 
+   }
+   else {
+    name.textContent = localStorage.getItem('name');
+   }
+ 
+} 
+
+
+//Get Focus
+  function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+     focus.textContent = '[Enter Focus]' ; 
+    }
+    else {
+    focus.textContent = localStorage.getItem('focus');
+    }
+ 
+} 
+
 
   //Run
   setBG();
   showTime();
+  getName();
+  getFocus();
